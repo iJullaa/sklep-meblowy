c5ee6249515d9a7f22f231c14bc7d123c4fc523d
iJullaa <sj53927@zut.edu.pl>
2025-09-27 13:13:23 +0200

    added working and tested user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import prisma from '../config/prisma.js';
 import bcrypt from 'bcryptjs';
+import { generateToken } from '../utils/jwt.js';
 
 export const registerUser = async (req, res, next) => {
   try {
@@ -33,6 +34,30 @@ export const registerUser = async (req, res, next) => {
       email: user.email,
       name: user.name,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        token: generateToken(user.id),
+      });
+    }
+    else {
+      res.status(401).json({ message: 'Nieprawidłowy email lub hasło' });
+    }
   } catch (error) {
     next(error);
   }
